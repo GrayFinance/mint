94fad074a38cfb19cc5f93df468820fc492f2823
leffw <93302536+leffw@users.noreply.github.com>
2022-01-24 23:25:40 -0300

    Initialize API and ZMQ

---
+++ src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/GrayFinance/mint/src/api"
+	"github.com/GrayFinance/mint/src/bitcoin"
+	"github.com/GrayFinance/mint/src/config"
+	"github.com/GrayFinance/mint/src/storage"
+)
+
+func init() {
+	if err := config.Loads(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := storage.Connect(config.Config.DATABASE); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	go bitcoin.Start()
+	api.Start()
+}
