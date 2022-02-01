e98d325adb9abe2f30d1947ac2a7b0c55725d4eb
leffw <93302536+leffw@users.noreply.github.com>
2022-02-01 20:42:32 -0300

    Connect a Redis

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -3,30 +3,33 @@ package storage
 import (
 	"strings"
 
+	"github.com/GrayFinance/mint/src/config"
 	"github.com/GrayFinance/mint/src/models"
+	"github.com/go-redis/redis"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-var err error
+var (
+	REDIS *redis.Client
+	DB    *gorm.DB
+	err   error
+)
 
-func Connect(uri string) error {
-	options := &gorm.Config{
-		SkipDefaultTransaction: true,
-		PrepareStmt:            true,
-	}
-	if strings.HasPrefix(uri, "postgres") {
-		DB, err = gorm.Open(postgres.Open(uri), options)
+func Connect() error {
+	// Connect database with ORM.
+	if strings.HasPrefix(config.Config.DATABASE, "postgres") {
+		DB, err = gorm.Open(postgres.Open(config.Config.DATABASE))
 	} else {
-		DB, err = gorm.Open(sqlite.Open(uri), options)
+		DB, err = gorm.Open(sqlite.Open(config.Config.DATABASE))
 	}
 
 	if err != nil {
 		return err
 	}
 
+	// Migration database.
 	err = DB.AutoMigrate(
 		&models.User{},
 		&models.Wallet{},
@@ -34,6 +37,17 @@ func Connect(uri string) error {
 		&models.Address{},
 	)
 	if err != nil {
+		return err
+	}
+
+	// Redis Connection
+	REDIS = redis.NewClient(&redis.Options{
+		Addr:     config.Config.REDIS_HOST,
+		Password: config.Config.REDIS_PASSWORD,
+		DB:       0,
+	})
+
+	if _, err := REDIS.Ping().Result(); err != nil {
 		return err
 	}
 	return nil
