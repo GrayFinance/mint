256f4633e6ee7e2dbe959b756d1c445b433772ca
leffw <93302536+leffw@users.noreply.github.com>
2022-02-05 21:59:07 -0300

    Update config.go

--- src/config/config.go
+++ src/config/config.go
@@ -5,23 +5,19 @@ import "github.com/kelseyhightower/envconfig"
 // Loads the variables of environments specified
 // in the .env file of the current directory.
 var Config struct {
-	API_HOST string `envconfig:"API_HOST"`
-	API_PORT string `envconfig:"API_PORT"`
-
-	DATABASE       string `envconfig:"DATABASE"`
-	REDIS_HOST     string `envconfig:"REDIS_HOST"`
-	REDIS_PASSWORD string `envconfig:"REDIS_PASSWORD"`
-
-	SIGN_KEY string `envconfig:"SIGN_KEY"`
-
+	API_HOST        string `envconfig:"API_HOST"`
+	API_PORT        string `envconfig:"API_PORT"`
+	DATABASE        string `envconfig:"DATABASE"`
+	REDIS_HOST      string `envconfig:"REDIS_HOST"`
+	REDIS_PASSWORD  string `envconfig:"REDIS_PASSWORD"`
+	SIGN_KEY        string `envconfig:"SIGN_KEY"`
 	BTC_HOST        string `envconfig:"BTC_HOST"`
 	BTC_USER        string `envconfig:"BTC_USER"`
 	BTC_PASS        string `envconfig:"BTC_PASS"`
 	BTC_ZMQ_HASH_TX string `envconfig:"BTC_ZMQ_HASH_TX"`
-
-	LND_HOST     string `envconfig:"LND_HOST"`
-	LND_MACAROON string `envconfig:"LND_MACAROON"`
-	LND_TLS_CERT string `envconfig:"LND_TLS_CERT"`
+	LND_HOST        string `envconfig:"LND_HOST"`
+	LND_MACAROON    string `envconfig:"LND_MACAROON"`
+	LND_TLS_CERT    string `envconfig:"LND_TLS_CERT"`
 }
 
 func Loads() error {
